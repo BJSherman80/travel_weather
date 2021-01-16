a58523537d26a9a78206dc9227017e8768271b20
Brett Turing <bjsherman80@gmail.com>
2021-01-16 14:50:12 -0700

    create method to get lat long as floats

--- app/services/geo_service.rb
+++ app/services/geo_service.rb
@@ -3,10 +3,10 @@ class GeoService
     conn = Faraday.new('https://www.mapquestapi.com')
     response = conn.get("/geocoding/v1/address?key=#{ENV['GEO_KEY']}&inFormat=kvp&outFormat=json&location=#{location}&thumbMaps=false")
     json = JSON.parse(response.body, symbolize_names: true)
-    x = lat_long_parsed(json)
+    lat_long_parsed(json)
   end
 
   def self.lat_long_parsed(json)
-    "#{json[:results][0][:locations][0][:latLng][:lat]}, #{json[:results][0][:locations][0][:latLng][:lng]}"
+    [(json[:results][0][:locations][0][:latLng][:lat]).to_f, (json[:results][0][:locations][0][:latLng][:lng]).to_f]
   end
 end
