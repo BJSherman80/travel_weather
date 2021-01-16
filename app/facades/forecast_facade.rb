93dc2baaed2be91380c1b54de4bfdde7cb58b04d
Brett Turing <bjsherman80@gmail.com>
2021-01-16 16:22:32 -0700

    add hourly weather and methods to limit daily and hourly weather

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,15 +4,26 @@ class ForecastFacade
     forecast_data = ForecastService.fetch_weather(lat_long)
     current_weather = CurrentWeather.new(forecast_data[:current])
     daily_weather = daily(limit_daily_5(forecast_data))
+    hourly_weather = hourly(limit_hourly_8(forecast_data))
   end
 
-  def self.limit_daily_5(data)
-    data[:daily][0..4]
+  def self.limit_daily_5(_data)
+    _data[:daily][0..4]
   end
 
-  def self.daily(five_day_forcast)
-    five_day_forcast.map do |data|
+  def self.limit_hourly_8(_data)
+    _data[:hourly][0..7]
+  end
+
+  def self.daily(_five_day_forecast)
+    _five_day_forecast.map do |data|
       DailyWeather.new(data)
+    end
+  end
+
+  def self.hourly(_eight_hour_forecast)
+    _eight_hour_forecast.map do |data|
+      HourlyWeather.new(data)
     end
   end
 end
