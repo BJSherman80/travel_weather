f5a614fc51d8c965d4b6d112e95781afe5d02102
Brett Turing <bjsherman80@gmail.com>
2021-01-16 14:49:28 -0700

    change route to show action to meet standards

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :forecast do
-        get '/', to: 'search#index'
+        get '/', to: 'search#show'
       end
     end
   end
