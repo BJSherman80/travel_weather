6fd7d2413dc22e4ac388acd5f19a7d42ecbe61c5
Brett Turing <bjsherman80@gmail.com>
2021-01-16 15:40:35 -0700

    add daily weather to fetch_weather and create methods for limit 5 days and daily forcast

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,18 @@
 class ForecastFacade
   def self.fetch_weather(location)
     lat_long = GeoService.fetch_lat_long(location)
-    forcast_data = ForecastService.fetch_weather(lat_long)
-    current_weather = CurrentWeather.new(forcast_data[:current])
+    forecast_data = ForecastService.fetch_weather(lat_long)
+    current_weather = CurrentWeather.new(forecast_data[:current])
+    daily_weather = daily(limit_daily_5(forecast_data))
+  end
+
+  def self.limit_daily_5(data)
+    data[:daily][0..4]
+  end
+
+  def self.daily(five_day_forcast)
+    five_day_forcast.map do |data|
+      DailyWeather.new(data)
+    end
   end
 end
