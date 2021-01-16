7011c7fddbd92bea9090d8ba461b1201483e58ba
Brett Turing <bjsherman80@gmail.com>
2021-01-16 16:21:46 -0700

    create hourly weather poro

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,17 @@
+class HourlyWeather
+  attr_reader :time,
+              :temperature,
+              :wind_speed,
+              :wind_direction,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @date = Time.at(data[:dt])
+    @temperature = (data[:temp])
+    @wind_speed = (data[:wind_speed])
+    @wind_direction = data[:wind_direction]
+    @conditions = data[:weather][0][:description]
+    @icon = (data[:weather][0][:icon])
+  end
+end
