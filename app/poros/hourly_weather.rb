29208cf767577feec7502be964787c811084f124
Brett Turing <bjsherman80@gmail.com>
2021-01-17 11:29:36 -0700

    create method for cardinal direction

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -10,8 +10,18 @@ class HourlyWeather
     @time = Time.at(data[:dt])
     @temperature = (data[:temp])
     @wind_speed = (data[:wind_speed])
-    @wind_direction = data[:wind_direction]
+    @wind_direction = cardinal_direction(data[:wind_direction])
     @conditions = data[:weather][0][:description]
     @icon = (data[:weather][0][:icon])
+  end
+
+  def cardinal_direction(num)
+    if num.nil?
+      num = 'No Wind'
+    else
+      val = ((num / 22.5) + 0.5).to_i
+      arr = %w[N NNE NE ENE E ESE SE SSE S SSW SW WSW W WNW NW NNW]
+      arr[(val % 16)]
+    end
   end
 end
