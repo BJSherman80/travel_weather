d8e7646d1f515dc44a3b3a923d93e827bed67731
Brett Turing <bjsherman80@gmail.com>
2021-01-16 17:00:52 -0700

    rename index to show to meet standards

--- app/controllers/api/v1/forecast/search_controller.rb
+++ app/controllers/api/v1/forecast/search_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Forecast::SearchController < ApplicationController
-  def index
-    render json: ForcastFacade.fetch_weather(params[:location])
+  def show
+    render json: ForecastSerializer.new(ForecastFacade.fetch_weather(params[:location]))
   end
 end
