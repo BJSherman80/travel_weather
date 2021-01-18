8ecca27cd492855cae5e557eab4d6052acc7d9dd
Brett Turing <bjsherman80@gmail.com>
2021-01-17 19:37:23 -0700

    edit test to make sure unwanted data is not in respected json

--- spec/requests/api/v1/open_weather_spec.rb
+++ spec/requests/api/v1/open_weather_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Forcast api' do
-  it 'can get json from a request' do
+  it 'can get correct json from a request' do
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
 
@@ -13,6 +13,7 @@ describe 'Forcast api' do
     expect(weather['data']['type']).to eq('forecast')
     expect(weather['data']).to have_key('attributes')
     expect(weather['data']['attributes']).to be_a Hash
+    # current weather
     expect(weather['data']['attributes']).to have_key('current_weather')
     expect(weather['data']['attributes']['current_weather']).to be_a Hash
     expect(weather['data']['attributes']['current_weather']).to have_key('datetime')
@@ -35,6 +36,7 @@ describe 'Forcast api' do
     expect(weather['data']['attributes']['current_weather']['conditions']).to be_a String
     expect(weather['data']['attributes']['current_weather']).to have_key('icon')
     expect(weather['data']['attributes']['current_weather']['icon']).to be_a String
+    # daily weather
     expect(weather['data']['attributes']).to have_key('daily_weather')
     expect(weather['data']['attributes']['daily_weather']).to be_a Array
     expect(weather['data']['attributes']['daily_weather'][0]).to have_key('date')
@@ -51,6 +53,7 @@ describe 'Forcast api' do
     expect(weather['data']['attributes']['daily_weather'][0]['conditions']).to be_a String
     expect(weather['data']['attributes']['daily_weather'][0]).to have_key('icon')
     expect(weather['data']['attributes']['daily_weather'][0]['icon']).to be_a String
+    # hourly weather
     expect(weather['data']['attributes']).to have_key('hourly_weather')
     expect(weather['data']['attributes']['hourly_weather']).to be_a Array
     expect(weather['data']['attributes']['hourly_weather'][0]).to have_key('time')
@@ -65,6 +68,38 @@ describe 'Forcast api' do
     expect(weather['data']['attributes']['hourly_weather'][0]['conditions']).to be_a String
     expect(weather['data']['attributes']['hourly_weather'][0]).to have_key('icon')
     expect(weather['data']['attributes']['hourly_weather'][0]['icon']).to be_a String
+  end
+
+  it 'does not use this json from request' do
+    get '/api/v1/forecast?location=denver,co'
+    expect(response).to be_successful
+
+    weather = JSON.parse(response.body)
+    # current
+    expect(weather['data']['attributes']['current_weather']).to_not have_key('pressure')
+    expect(weather['data']['attributes']['current_weather']).to_not have_key('dew_point')
+    expect(weather['data']['attributes']['current_weather']).to_not have_key('clouds')
+    expect(weather['data']['attributes']['current_weather']).to_not have_key('wind_speed')
+    expect(weather['data']['attributes']['current_weather']).to_not have_key('wind_direction')
+    # daily
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('feels_like')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('pressure')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('humidity')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('dew_point')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('wind_speed')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('wind_direction')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('uvi')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('clouds')
+    expect(weather['data']['attributes']['daily_weather'][0]).to_not have_key('pop')
+    # hourly
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('pressure')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('feels_like')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('humidity')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('dew_point')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('uvi')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('clouds')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('visibility')
+    expect(weather['data']['attributes']['hourly_weather'][0]).to_not have_key('pop')
   end
 
   it 'can gets 5 day daily forcast and 8 hour hourly forecast' do
