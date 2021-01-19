cb65f705e1804c8b036fc4a6d517814d497e2217
Brett Turing <bjsherman80@gmail.com>
2021-01-18 18:31:22 -0700

    remove pry

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -11,7 +11,6 @@ class DailyWeather
     @date = Time.at(data[:dt]).in_time_zone
     @sunrise = Time.at(data[:sunrise]).in_time_zone
     @sunset = Time.at(data[:sunset]).in_time_zone
-    binding.pry
     @max_temp = data[:temp][:max]
     @min_temp = data[:temp][:min]
     @conditions = data[:weather][0][:description]
