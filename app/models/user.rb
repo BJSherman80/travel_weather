3feec454b99032870997ecabf4d741c4ae19e2df
Brett Turing <bjsherman80@gmail.com>
2021-01-19 06:19:50 -0700

    refactor model to check for password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
-  validates :password, confirmation: { case_sensitive: true }
+  validates_presence_of :password_confirmation
+  validates :password, confirmation: true
   validates :api_key, uniqueness: true
 end
