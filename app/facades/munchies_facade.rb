1e9cdd105f9504929a192289b8188b4b559ddb0f
Brett Turing <bjsherman80@gmail.com>
2021-01-19 10:04:01 -0700

    fixed poro

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,5 +1,13 @@
 class MunchiesFacade
   def self.fetch_data(location, _destination, food)
     resturant_data = YelpService.fetch_resturants(food, location)
+    resturant = Resturant.new(resturant_data)
+    binding.pry
+  end
+
+  def self.create_resturant(data)
+    data.map do |resturant|
+      Resturant.new(resturant)
+    end
   end
 end
--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,12 +1,13 @@
 class YelpService
   def self.fetch_resturants(food, location)
-    binding.pry
     conn = Faraday.new('https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['YELP_API_KEY']
-		end
-		response = conn.get("/v3/businesses/search") do |f|
-			f.params[:location] = location
-			f.params[:categories] = category
-		end
-		parsed_json = JSON.parse(response.body, symbolize_names: true)
-	end
+    end
+    response = conn.get('/v3/businesses/search') do |f|
+      f.params[:location] = location
+      f.params[:categories] = food
+      f.params[:limit] = 1
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
