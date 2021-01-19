1480b6583915de46457c93c7b5aa7aac846906da
Brett Turing <bjsherman80@gmail.com>
2021-01-19 11:12:53 -0700

    facade test passing

--- spec/requests/api/v1/munchies_spec.rb
+++ spec/requests/api/v1/munchies_spec.rb
@@ -5,7 +5,33 @@ describe 'Munchies api' do
     get '/api/v1/munchies?start=denver,co&destination=pueblo,co&food=chinese'
     expect(response).to be_successful
 
-    data = JSON.parse(response.body)
-    binding.pry
+    munchie_data = JSON.parse(response.body)
+
+    expect(munchie_data['data']).to have_key('id')
+    expect(munchie_data['data']['id']).to eq(nil)
+    expect(munchie_data['data']).to have_key('type')
+    expect(munchie_data['data']['type']).to eq('munchie')
+    expect(munchie_data['data']).to have_key('attributes')
+    expect(munchie_data['data']['attributes']).to be_a Hash
+    expect(munchie_data['data']['attributes']).to have_key('destination')
+    expect(munchie_data['data']['attributes']['destination']).to be_a Hash
+    expect(munchie_data['data']['attributes']['destination']).to have_key('city')
+    expect(munchie_data['data']['attributes']['destination']['city']).to be_a String
+    expect(munchie_data['data']['attributes']['destination']).to have_key('travel_time')
+    expect(munchie_data['data']['attributes']['destination']['travel_time']).to be_a String
+    expect(munchie_data['data']['attributes']).to have_key('current_weather')
+    expect(munchie_data['data']['attributes']['current_weather']).to be_a Hash
+    expect(munchie_data['data']['attributes']['current_weather']).to have_key('temperature')
+    expect(munchie_data['data']['attributes']['current_weather']['temperature']).to be_a(Integer).or be_a(Float)
+    expect(munchie_data['data']['attributes']['current_weather']).to have_key('summary')
+    expect(munchie_data['data']['attributes']['current_weather']['summary']).to be_a String
+    expect(munchie_data['data']['attributes']).to have_key('resturant')
+    expect(munchie_data['data']['attributes']['resturant']).to be_a Hash
+    expect(munchie_data['data']['attributes']['resturant']).to have_key('name')
+    expect(munchie_data['data']['attributes']['resturant']['name']).to be_a String
+    expect(munchie_data['data']['attributes']['resturant']).to have_key('address')
+    expect(munchie_data['data']['attributes']['resturant']['address']).to be_a Array
+    expect(munchie_data['data']['attributes']['resturant']['address'][0]).to be_a String
+    expect(munchie_data['data']['attributes']['resturant']['address'][1]).to be_a String
   end
 end
