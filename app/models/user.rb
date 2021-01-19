4c3af239e0b8871c7e93fa1772107e4bc536dc0a
Brett Turing <bjsherman80@gmail.com>
2021-01-18 19:34:30 -0700

    refactor syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_key
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates_presence_of :password
   validates :password, confirmation: { case_sensitive: true }
   validates :api_key, uniqueness: true
 end
