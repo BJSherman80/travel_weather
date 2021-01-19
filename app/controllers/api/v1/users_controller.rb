3bda7e6b17e1191a4ede17bffd6c3761699dd05f
Brett Turing <bjsherman80@gmail.com>
2021-01-18 19:34:17 -0700

    refactor method to account for password confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    if user.save
+    if !params[:password].empty? && params[:password] != params[:password_confirmation]
+      render json: { errors: 'The password confirmation does not match.' }, status: 409
+    elsif user.save
       render json: UsersSerializer.new(user), status: 201
     else
-      render json: { errors: user.errors.full_messages.to_sentence }, status: 400
+      render json: { errors: user.errors.full_messages.first }, status: 400
     end
   end
 
