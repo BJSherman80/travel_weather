151ebe5bd6ddb6d05ca4ea3bac3416daba13403f
Brett Turing <bjsherman80@gmail.com>
2021-01-19 11:47:57 -0700

    fixed so resturant is in destination city

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,6 +1,6 @@
 class MunchiesFacade
   def self.fetch_data(location, destination, food)
-    resturant_data = YelpService.fetch_resturants(food, location)
+    resturant_data = YelpService.fetch_resturants(food, destination)
     resturant = Resturant.new(resturant_data)
     travel_data = GeoService.fetch_travel_data(location, destination)
     destination = Destination.new(travel_data)
--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,10 +1,10 @@
 class YelpService
-  def self.fetch_resturants(food, location)
+  def self.fetch_resturants(food, destination)
     conn = Faraday.new('https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['YELP_API_KEY']
     end
     response = conn.get('/v3/businesses/search') do |f|
-      f.params[:location] = location
+      f.params[:location] = destination
       f.params[:categories] = food
       f.params[:limit] = 1
     end
