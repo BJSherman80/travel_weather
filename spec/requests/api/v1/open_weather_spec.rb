61db0a4492f85e8a41096b17c455ae15a8efb447
Brett Turing <bjsherman80@gmail.com>
2021-01-28 13:54:45 -0700

    remove pry

--- spec/requests/api/v1/open_weather_spec.rb
+++ spec/requests/api/v1/open_weather_spec.rb
@@ -6,7 +6,7 @@ describe 'Forcast request' do
     expect(response).to be_successful
 
     weather = JSON.parse(response.body)
-    binding.pry
+
     expect(weather['data']).to have_key('id')
     expect(weather['data']['id']).to eq(nil)
     expect(weather['data']).to have_key('type')
